28e6a0302f76c7c10ef8e96867f1830d6e9559ac
akarshippili <ippiliakarsh@gmail.com>
2025-07-24 02:36:32 +0530

    basic server

--- build.sbt
+++ build.sbt
@@ -9,7 +9,10 @@ lazy val root = (project in file("."))
   .settings(
     name := "learning-redis",
     libraryDependencies += munit % Test,
-    libraryDependencies += "redis.clients" % "jedis" % "5.1.0"
+    libraryDependencies += "redis.clients" % "jedis" % "5.1.0",
+    libraryDependencies += "com.typesafe.akka" %% "akka-http" % "10.5.3",
+    libraryDependencies += "com.typesafe.akka" %% "akka-stream" % "2.8.5",
+    libraryDependencies += "com.typesafe.akka" %% "akka-actor" % "2.8.5"
   )
 
 // See https://www.scala-sbt.org/1.x/docs/Using-Sonatype.html for instructions on how to publish to Sonatype.
--- src/main/scala/com/akarsh/HttpServer.scala
+++ src/main/scala/com/akarsh/HttpServer.scala
@@ -1,3 +1,28 @@
 package com.akarsh
 
-object HttpServer extends App {}
+import akka.actor.{ActorSystem, Props}
+import scala.io.StdIn
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route.seal
+import scala.concurrent.ExecutionContext
+
+object HttpServer extends App {
+    implicit val actorSystem: ActorSystem = ActorSystem("HttpServer")
+    implicit val executionContext: ExecutionContext = actorSystem.dispatcher
+
+    val route = path("hello") {
+        get {
+            complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Hello, World!</h1>"))
+        }
+    }
+
+    val bindingFuture = Http().newServerAt("localhost", 8080).bind(route)
+    println(s"Server online at http://localhost:8080/hello")
+
+    StdIn.readLine()
+    bindingFuture
+      .flatMap(_.unbind())
+      .onComplete(_ => actorSystem.terminate())
+}
